f8e2d1e65e41f00fc74ebb9a83e3a6713b142f90
okunlade Jolade <jokunlade@gmail.com>
2022-05-22 01:53:29 +0100

    i am excited

--- scripts/run.js
+++ scripts/run.js
@@ -1,11 +1,21 @@
-const { hexStripZeros } = require("ethers/lib/utils")
+
 
 const main = async () => {
+    const [owner, randomPerson] = await hre.ethers.getSigners();
     const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
     const waveContract = await waveContractFactory.deploy();
     await waveContract.deployed();
-    console.log("Contract deployed to:", waveContract.address);
 
+    console.log("Contract deployed to:", waveContract.address);
+    console.log("contract deployed by:", owner.address);
+    
+    let waveCount;
+    waveCount = await waveContract.getTotalWaves();
+
+    let waveTxn = await waveContract.wave();
+    await waveTxn.wait();
+
+    waveCount = await waveContract.getTotalWaves();
 };
 
 const runMain = async () => {

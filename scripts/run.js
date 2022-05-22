8a132d1465e269b2c9cd960f5c446cda15f9a4d4
okunlade Jolade <jokunlade@gmail.com>
2022-05-22 02:02:28 +0100

    ading more functions

--- scripts/run.js
+++ scripts/run.js
@@ -16,6 +16,11 @@ const main = async () => {
     await waveTxn.wait();
 
     waveCount = await waveContract.getTotalWaves();
+
+    waveTxn = await waveContract.connect(randomPerson).wave();
+    await waveTxn.wait();
+
+    waveCount = await waveContract.getTotalWaves();
 };
 
 const runMain = async () => {

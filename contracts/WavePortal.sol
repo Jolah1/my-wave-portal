f6334416a0d8843305cd0b2cff0abc72c221c382
okunlade Jolade <jokunlade@gmail.com>
2022-05-22 01:00:08 +0100

    i am excited

---
+++ contracts/WavePortal.sol
@@ -0,0 +1,10 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract WavePortal {
+    constructor() {
+        console.log("Yo yo, I am contract and I am a smart Jolade");
+    }
+}

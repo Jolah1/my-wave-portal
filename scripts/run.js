67b090c9d3e9dbf62ba911fccb31c0808a605bf6
okunlade Jolade <jokunlade@gmail.com>
2022-05-26 23:08:36 +0100

    more update

--- scripts/run.js
+++ scripts/run.js
@@ -1,21 +1,30 @@
 
 const main = async () => {
     const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
-    const waveContract = await waveContractFactory.deploy();
+    const waveContract = await waveContractFactory.deploy({
+        value: hre.ethers.utils.parseEther("0.1"),
+    });
     await waveContract.deployed();
 
     console.log("Contract addy:", waveContract.address);
     
-    let waveCount;
-    waveCount = await waveContract.getTotalWaves();
-    console.log(waveCount.toNumber());
+    let contractBalance = await hre.ethers.provider.getBalance(
+        waveContract.address
+    );
+    console.log(
+        "Contract balance:",
+        hre.ethers.utils.formatEther(contractBalance)
+    );
 
     let waveTxn = await waveContract.wave("A message!");
     await waveTxn.wait();
 
-    const [_, randomPerson] = await hre.ethers.getSigners();
-    waveTxn = await waveContract.connect(randomPerson).wave("Another message!");
-    await waveTxn.wait();
+    contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
+    console.log(
+        "Contract balance:",
+        hre.ethers.utils.formatEther(contractBalance)
+    );
+
 
     let allWaves = await waveContract.getAllWaves();
     console.log(allWaves);
